fa4f47a0e34ed79469c9eecafc34c6080dfffbbf
bmaggiano <brandon.maggiano@gmail.com>
2022-10-27 14:31:40 -0700

    added to routes

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -1,13 +1,39 @@
 const router = require('express').Router();
-const { Comments } = require('../../models')
+const { Comments, User } = require('../../models')
+
+//displays comments 
+router.get('/comments', async (req, res) => {
+    try {
+        const commentData = await Comments.findAll({
+            indlude: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const comments = commentData.map((gallery) =>
+        gallery.get({ plain: true })
+      );
+
+      res.render(//need reference to handlebar, {
+        //comments }
+      );
+    } catch {
+        res.status(500).json(err);
+    }
+});
 
 //Creates new comment
-// router.post('/comments', async (req, res) => {
-//     try {
-//         const comment = 
-//     } catch {
+router.post('/comments', async (req, res) => {
+    try {
+        const comment = await Comments.create({
+            username: req.body.username
+        })
+    } catch {
 
-//     }
-// });
+    }
+});
 
 module.exports = router;
