0ad43d1baca489f4d8644a4500e859133eb4aa32
MG <magallardo11@outlook.com>
2022-10-27 10:49:37 -0700

    usergames model complete

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Comments = require("./Comments")
+const Games = require("./Games")
+const User = require("./User")
+const Usergames = require("./Usergames")
+
+module.exports = {
+  Comments,  
+  Games,
+  User,
+  Usergames
+  };
