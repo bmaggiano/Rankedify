c7fc9dab63254aef76221496bea08e7b642a232a
bmaggiano <brandon.maggiano@gmail.com>
2022-10-27 11:58:27 -0700

    initial /login test passed

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const { Comments } = require('../../models')
 
 //Creates new comment
-router.post('/comments', async (req, res) => {
-    try {
-        const comment = 
-    } catch {
+// router.post('/comments', async (req, res) => {
+//     try {
+//         const comment = 
+//     } catch {
 
-    }
-});
+//     }
+// });
 
 module.exports = router;
