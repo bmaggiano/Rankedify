389c4ceb30f89fe68515e7286439d59985316fbd
MG <magallardo11@outlook.com>
2022-10-27 11:41:35 -0700

    models near complete

--- models/index.js
+++ models/index.js
@@ -13,14 +13,15 @@ User.hasMany(Usergames, {
   foreignKey: 'user_id',
   onDelete: "CASCADE"
 });
+
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
+Games.belongsToMany(User, {
+  through: Usergames,
   foreignKey: 'user_id',
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag,
+User.belongsToMany(Games, {
+  through: Usergames,
   foreignKey: 'game_id',
 });
 
