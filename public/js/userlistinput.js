1e8de5d660145a2e49b6c90cf2b3b06b504598f7
MG <magallardo11@outlook.com>
2022-10-30 14:07:51 -0700

    userlist

---
+++ public/js/userlistinput.js
@@ -0,0 +1,57 @@
+const userInput = document.getElementById('userInput')
+const loginFormHandler = async (event) => {event.preventDefault();
+}
+
+
+
+const userInputOne = document.querySelector("input-one");
+
+userInputOne.addEventListener("input", () => {
+});
+
+userInputOne.addEventListener("invalid", () => {
+  userInputOne.setCustomValidity("Please fill in your First Game.");
+});
+
+const userInputTwo = document.querySelector("input-two");
+
+userInputTwo.addEventListener("input", () => {
+});
+
+userInputTwo.addEventListener("invalid", () => {
+  userInputTwo.setCustomValidity("Please fill in your Second Game.");
+});
+
+const userInputThree = document.querySelector("input-three");
+
+userInputThree.addEventListener("input", () => {
+});
+
+userInputThree.addEventListener("invalid", () => {
+  userInputThree.setCustomValidity("Please fill in your Third Game.");
+});
+
+const userInputFour = document.querySelector("input-four");
+
+userInputFour.addEventListener("input", () => {
+});
+
+userInputFour.addEventListener("invalid", () => {
+  userInputFour.setCustomValidity("Please fill in your Fourth Game.");
+});
+
+const userInputFive = document.querySelector("input-five");
+
+userInputFive.addEventListener("input", () => {
+});
+
+userInputFive.addEventListener("invalid", () => {
+  userInputFive.setCustomValidity("Please fill in your Fifth Game.");
+});
+
+
+// const userList = document.getElementById('userInput').value.trim();
+    
+// if (userInput.value) {
+//     const userInputOne = document.getElementById('input-one')
+// }
